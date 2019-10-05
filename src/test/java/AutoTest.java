cd04db1f28159999fd43aa5a0a305bcc252fc516
Balakhonov Andrey <balakhonovandrey@gmail.com>
2019-10-05 03:14:47 +0300

    First commit

---
+++ src/test/java/AutoTest.java
@@ -0,0 +1,56 @@
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class AutoTest extends BaseTest {
+
+    @Before
+    public void startTest() {
+        System.out.println("startTest");
+    }
+
+    @After
+    public void stopTest() {
+        System.out.println("stopTest");
+    }
+
+    @Test
+    public void test1() throws InterruptedException {
+
+        driver.get(url);
+        findAndClick(findMenu(menu,"Страхование"));
+        findAndClick(findMenu(subMenu, "Страхование путешественников"));
+
+        Assert.assertEquals("Проверка заголовка: "
+                ,"Страхование путешественников"
+                , findElement("//div[@class='sbrf-rich-outer']/h1").getText());
+
+        findAndClick("//span[text()='Оформить онлайн']");
+        findAndClick("//img[@src='/portalserver/content/atom/contentRepository/content/person/travel/banner-zashita-traveler.jpg?id=f6c836e1-5c5c-4367-b0d0-bbfb96be9c53']");
+
+//        (new WebDriverWait(driver, 10))
+//                .until(ExpectedConditions.elementToBeClickable(By
+//                        .xpath("//div[@class='b-form-box-title ng-binding' and text()='Минимальная']"))).click();
+
+        Thread.sleep(5000); // в 3 часа ночи ничего лучше на ум не приходит((
+//        driver.findElement(By.xpath("//div[@class='b-form-box-title ng-binding' and text()='Минимальная']"));
+
+        findAndClick("//*[@class='b-button-block-center']/span");
+
+//        driverClose();
+        }
+
+}
+
+
+
+//        (new WebDriverWait(driver, 10)).until(new ExpectedCondition<WebDriver>() {
+//            public WebDriver apply(WebDriver d) {
+//                d.get("https://online.sberbankins.ru/store/vzr/index.html#/viewCalc");
+//                return d;
+//       }});
---
+++ src/test/java/BaseTest.java
@@ -0,0 +1,60 @@
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+
+public class BaseTest {
+
+    WebDriver driver = null;
+    WebElement element = null;
+
+    String url = "https://www.sberbank.ru/ru/person";
+    String menu = "//button[@class='lg-menu__link' and @aria-label='Меню %s']";
+    String subMenu = "//*[@class='lg-menu__sub-link' and text()='%s']";
+    String polisCost = "//div[@class='b-form-box-title ng-binding' and text()='%s']";
+
+    @Before
+    public void startBrowser() {
+//        String browser = System.getProperty("browser", "firefox");
+//        switch (browser) {
+//            case "firefox": {
+                this.driver = new FirefoxDriver();
+//                break;
+//            }
+//            case "chrome": {
+//                this.driver = new ChromeDriver();
+//            }
+//        }
+    }
+
+    public void driverClose() {
+        this.driver.quit();
+    }
+
+    public WebElement waitDynamicElement (String xpath) {
+        this.element = (new WebDriverWait(this.driver, 10))
+                .until(ExpectedConditions.presenceOfElementLocated(By.xpath(xpath)));
+        return element;
+    }
+
+    public WebElement findElement(String xpath) {
+        return this.element = this.driver.findElement(By.xpath(xpath));
+    }
+
+    public WebElement findAndClick(String xpath) {
+        this.element = this.driver.findElement(By.xpath(xpath));
+        this.element.click();
+        return element;
+    }
+
+    public String findMenu(String menu, String menuName) {
+        return String.format(menu, menuName);
+    }
+
+}
+
